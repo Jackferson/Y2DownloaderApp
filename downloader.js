2758e49c0c8485f3521a30ff67911aa91d16aac3
Jack <Jackferson15@gmail.com>
2022-08-18 23:37:13 -0400

    Process and backend done, missing css and frontend

--- downloader.js
+++ downloader.js
@@ -1,14 +1,30 @@
-const fs = require("fs");
 const ytdl = require("ytdl-core");
+const ffmpeg = require("fluent-ffmpeg");
 
-const download = async (songsList) => {
-  while (songsList.length > 0) {
-    let string = songsList[0].name;
-    let realName = string.replace(/[^a-zA-Z0-9]/g, "-");
-    ytdl(songsList[0].videoId, { quality: "highestaudio" }).pipe(
-      fs.createWriteStream(`${realName}.mp3`)
-    );
-    songsList.shift();
+let index = 0;
+
+const download = async (songList) => {
+  let item = songList[index];
+  let string = item.name;
+  let realName = string.replace(/[^a-zA-Z0-9áéíóúñ ]/g, "-");
+  const stream = ytdl(item.videoId, { quality: "highestaudio" });
+  ffmpeg(stream)
+    .audioBitrate(128)
+    .save(`${__dirname}/downloads/${realName}.mp3`)
+    .on("end", () => {
+      console.log(`${realName}`);
+      finish(songList);
+    });
+};
+
+const finish = async (songList) => {
+  if (index < songList.length - 1) {
+    index++;
+    console.log("finish");
+    await download(songList);
+  } else {
+    console.log("Done");
   }
 };
+
 exports.download = download;
